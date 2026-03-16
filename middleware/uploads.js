49fe2d6b4bfe536e1bec28654aebe3d7159890fd
Manjeet Kumar <manjeetgodara001@gmail.com>
2026-03-16 15:43:33 +0530

    cloudanry added

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,13 +1,20 @@
 import multer from "multer";
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const upload = multer({ storage: storage });
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "nielit",
+    allowed_formats: ["jpg", "jpeg", "png", "pdf", "webp"],
+    resource_type: "auto",
+  },
+});
+
+export const upload = multer({ storage });
